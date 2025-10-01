a2f99595741ffdf0c105399166c2819e39d805d1
02loveslollipop <mmonsalve2005@gmail.com>
2025-09-30 21:55:45 -0500

    feat: Remove unused API handler for dynamic slug redirection

--- pages/api/[...slug].ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { slug } = req.query;
-  const path = Array.isArray(slug) ? slug.join("/") : slug;
-  const blobUrl = `https://nt9pzjxsvf6ahuq3.public.blob.vercel-storage.com/${path}`;
-  res.redirect(blobUrl);
-}
